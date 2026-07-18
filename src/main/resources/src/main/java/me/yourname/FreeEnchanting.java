58a0015321fa75e9ee6052f2dcf1dac4ebfbb668
That1GuyyyY <michael.jarvie2012@gmail.com>
2026-07-19 09:51:41 +1000

    Create FreeEnchanting.java

---
+++ src/main/resources/src/main/java/me/yourname/FreeEnchanting.java
@@ -0,0 +1,10 @@
+package me.yourname;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FreeEnchanting extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new EnchantListener(), this);
+    }
+}
