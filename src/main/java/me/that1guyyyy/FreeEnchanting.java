3f2d91f0c966ab9467b80783530a66bb5355d4ca
That1GuyyyY <michael.jarvie2012@gmail.com>
2026-07-19 11:47:37 +1000

    Update FreeEnchanting.java

--- src/main/java/me/that1guyyyy/FreeEnchanting.java
+++ src/main/java/me/that1guyyyy/FreeEnchanting.java
@@ -1,10 +1,34 @@
 package me.that1guyyyy;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FreeEnchanting extends JavaPlugin {
+public class FreeEnchanting extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new EnchantListener(), this);
+        saveDefaultConfig(); 
+        getServer().getPluginManager().registerEvents(new EnchantListener(this), this);
+        getCommand("noxp").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command!");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        if (!player.isOp()) {
+            player.sendMessage("§cYou do not have permission to use this command!");
+            return true;
+        }
+
+        EnchantListener.openSettingsMenu(player);
+        return true;
     }
 }
